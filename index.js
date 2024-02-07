52e245366067cce0cba7f294f8513cfea83e3419
Fabinho <116457022+falopinho@users.noreply.github.com>
2024-02-07 00:30:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const input = document.querySelector("input")
+
+
+//a character limit
+input.addEventListener("input", ()=>{
+    input.value = input.value.substring (0, 8)
+})
+
+// just get 0 and 1 
+function lettersOnly(input){
+    var regex = /[^0-1]/gi;
+    input.value = input.value.replace(regex,"");
+}
+
+function converter(num) {
+    var bin = parseInt(num, 2)
+    document.getElementById("res").innerText=bin
+}
